d45d32fe058c785990ec012c33566a0eeb72ae51
michael <michael.thenault@gmail.com>
2018-12-26 17:21:52 +0100

    remove use of global

--- src/micpyplayer.py
+++ src/micpyplayer.py
@@ -11,27 +11,11 @@ from pathlib import Path
 import track_player
 import play_queue
 
-curdir_path = None
-offset_filelist = 0
-selected_line = 1
-coord_max_y = None
-file_list_in_current_dir = None
-exit_requested = False
 logger = logging.getLogger("micpyplayer")
 logger.setLevel(logging.DEBUG)
-show_menu = False
 main_box = None
 
-
 def main(stdscr):
-
-    global curdir_path
-    global offset_filelist
-    global selected_line
-    global coord_max_y
-    global file_list_in_current_dir
-    global exit_requested
-    global main_box
     known_extensions = '.mp3', '.wav', '.aac', '.aif', '.ogg', '.wma', '.flac', '.m4a', '.Mp3', '.MP3'
 
     arg_path = ""
@@ -40,7 +24,9 @@ def main(stdscr):
     else:
         arg_path = sys.argv[1]
 
-    curdir_path = Path(arg_path)
+    current_app_state = []
+    current_app_state.append(AppState(Path(arg_path), 0, 1))
+    current_app_state.append(threading.Lock())
 
     ch = logging.FileHandler("output_log.txt")
     ch.setLevel(logging.DEBUG)
@@ -57,19 +43,17 @@ def main(stdscr):
     screen.box()
     screen.hline(2, 1, curses.ACS_HLINE, 77)
     screen.refresh()
-    main_box = MainBox(stdscr)
+    main_box = MainBox(stdscr, current_app_state)
     # max_y, max_x = stdscr.getmaxyx()
     our_player = track_player.TrackPlayer(logger)
     thread_started = False
-    refresher = ScreenPainter(
-        stdscr, main_box, our_player, known_extensions)
+    refresher = ScreenPainter(stdscr, main_box, our_player, known_extensions, current_app_state )
 
-    while not exit_requested:
+    while not current_app_state[0].exit_requested:
         refresher.refresh()
 
         if not thread_started:
-            t1 = threading.Thread(target=get_input, args=(
-                our_player, stdscr, refresher))
+            t1 = InputClass(our_player, stdscr, refresher, current_app_state)
             t1.start()
             logger.debug("thread started")
             thread_started = True
@@ -77,8 +61,21 @@ def main(stdscr):
         time.sleep(0.48)
 
 
+class AppState(object):
+    def __init__(self, curdir_path, offset_filelist, selected_line):
+        self.curdir_path = curdir_path
+        self.offset_filelist = offset_filelist
+        self.selected_line = selected_line
+        self.coord_max_y = None
+        self.file_list_in_current_dir = None
+        self.exit_requested = False
+        self.show_menu = False
+
+
+
 class MainBox(object):
-    def __init__(self, stdscr):
+    def __init__(self, stdscr, current_application_state):
+        self.current_application_state = current_application_state
         self.update(stdscr)
         self.menu_x_size = 20
         self.min_x = None
@@ -86,8 +83,7 @@ class MainBox(object):
         self.max_x = None
 
     def update(self, stdscr):
-        global show_menu
-        if show_menu:
+        if self.current_application_state[0].show_menu:
             self.min_x = self.menu_x_size
         else:
             self.min_x = 1
@@ -97,18 +93,14 @@ class MainBox(object):
 
 class ScreenPainter(object):
 
-    def __init__(self, stdscr, main_box, our_player, known_extensions):
+    def __init__(self, stdscr, main_box, our_player, known_extensions, current_app_state):
         self.stdscr = stdscr
         self.main_box = main_box
         self.our_player = our_player
         self.known_extensions = known_extensions
+        self.current_app_state = current_app_state
 
     def refresh(self):
-        global curdir_path
-        global offset_filelist
-        global selected_line
-        global coord_max_y
-        global file_list_in_current_dir
 
         # logger.debug("refresh")
         self.main_box.update(self.stdscr)
@@ -118,7 +110,7 @@ class ScreenPainter(object):
         self.stdscr.vline(self.main_box.max_y - 4, 1,
                           curses.ACS_HLINE, self.main_box.max_x - 2)
 
-        coord_max_y = self.main_box.max_y - 5
+        self.current_app_state[0].coord_max_y = self.main_box.max_y - 5
 
         self.stdscr.hline(self.main_box.max_y - 4, 1,
                           curses.ACS_HLINE, self.main_box.max_x - 2)
@@ -130,20 +122,19 @@ class ScreenPainter(object):
 
         self.draw_path_on_top()
 
-        self.draw_file_list(coord_max_y)
+        self.draw_file_list(self.current_app_state[0].coord_max_y)
 
         self.stdscr.refresh()
 
     def draw_path_on_top(self):
-        arg_path_print = str(curdir_path)[max(
-            0, (len(str(curdir_path)) + 4) - self.main_box.max_x):]
+        arg_path_print = str(self.current_app_state[0].curdir_path)[max(
+            0, (len(str(self.current_app_state[0].curdir_path)) + 4) - self.main_box.max_x):]
 
         self.stdscr.addstr(0, int(self.main_box.max_x / 2) - int(((len(arg_path_print) + 2) / 2)),
                            "|" + arg_path_print + "|")
 
     def draw_menu(self):
-        global show_menu
-        if show_menu:
+        if self.current_app_state[0].show_menu:
             self.stdscr.vline(1, self.main_box.menu_x_size-1,
                               curses.ACS_VLINE, self.main_box.max_y - 5)
             self.stdscr.addstr(1, 2, "Menu", curses.A_UNDERLINE)
@@ -152,29 +143,25 @@ class ScreenPainter(object):
             self.stdscr.addstr(4, 2, "Themes")
 
     def draw_file_list(self, coord_max_y):
-        global curdir_path
-        global offset_filelist
-        global selected_line
-        global file_list_in_current_dir
 
-        file_list_in_current_dir = [s for s in os.listdir(str(curdir_path))
+        self.current_app_state[0].file_list_in_current_dir = [s for s in os.listdir(str(self.current_app_state[0].curdir_path))
                                     if (s.endswith(self.known_extensions) or
-                                        os.path.isdir(str(curdir_path) + "/" + s))
+                                        os.path.isdir(str(self.current_app_state[0].curdir_path) + "/" + s))
                                     and not s.startswith(".")]
 
-        file_list_in_current_dir.insert(0, "..")
+        self.current_app_state[0].file_list_in_current_dir.insert(0, "..")
 
-        for line, f in enumerate(file_list_in_current_dir[offset_filelist:coord_max_y
-                                                          + offset_filelist]):
+        for line, f in enumerate(self.current_app_state[0].file_list_in_current_dir[self.current_app_state[0].offset_filelist:coord_max_y
+                                                          + self.current_app_state[0].offset_filelist]):
 
-            if curdir_path.joinpath(Path(f)).is_dir() and line > 0:
+            if self.current_app_state[0].curdir_path.joinpath(Path(f)).is_dir() and line > 0:
                 f = f + "/"
             # coloring selection
-            if selected_line == 1 + line:
+            if self.current_app_state[0].selected_line == 1 + line:
                 self.stdscr.addstr(
-                    1 + line, self.main_box.min_x, f[:self.main_box.max_x - 2], curses.A_REVERSE)
+                    1 + line, self.main_box.min_x, " " + f[:self.main_box.max_x - 2], curses.A_REVERSE)
             else:
-                self.stdscr.addstr(1 + line, self.main_box.min_x, f, 0)
+                self.stdscr.addstr(1 + line, self.main_box.min_x, " " +f, 0)
 
     def draw_status(self):
         # print status for our_player
@@ -191,80 +178,76 @@ class ScreenPainter(object):
                 self.main_box.max_y - 2, 1, line_2_and_bars[:self.main_box.max_x - 3], curses.A_REVERSE)
 
 
-def get_input(our_player, stdscr, refresher):
+class InputClass(threading.Thread):
+    def __init__(self, our_player, stdscr, refresher, current_app_state):
 
-    logger.debug("inside thread ")
-    global curdir_path
-    global offset_filelist
-    global selected_line
-    global coord_max_y
-    global file_list_in_current_dir
-    global exit_requested
-    global show_menu
+        self.our_player = our_player
+        self.stdscr = stdscr
+        self.refresher = refresher
+        self.current_app_state = current_app_state
+        self.get_input()
+        threading.Thread.__init__(self)
 
-    while not exit_requested:
-        got_key = stdscr.getch()
-        if got_key == curses.KEY_UP:
-            process_up()
-        elif got_key == curses.KEY_DOWN:
-            process_down(coord_max_y, file_list_in_current_dir)
-        elif got_key == curses.KEY_RIGHT:
-            process_right(file_list_in_current_dir, our_player)
-        elif got_key == ord('q'):
-            exit_requested = True
-            break
-        elif got_key == ord('m'):
-            show_menu = not show_menu
-        elif got_key == ord('+'):
-            logger.debug("volume +")
-        elif got_key == ord('-'):
-            logger.debug("volume -")
-        refresher.refresh()
-    # return curdir_path, offset_filelist, selected_line
+    def get_input(self):
+        logger.debug("inside thread ")
 
+        while not self.current_app_state[0].exit_requested:
+            got_key = self.stdscr.getch()
+            if got_key == curses.KEY_UP:
+                self.process_up()
+            elif got_key == curses.KEY_DOWN:
+                self.process_down(self.current_app_state[0].coord_max_y, self.current_app_state[0].file_list_in_current_dir)
+            elif got_key == curses.KEY_RIGHT:
+                self.process_right(self.current_app_state[0].file_list_in_current_dir, self.our_player)
+            elif got_key == ord('q'):
+                self.current_app_state[0].exit_requested = True
+                break
+            elif got_key == ord('m'):
+                self.current_app_state[0].show_menu = not self.current_app_state[0].show_menu
+            elif got_key == ord('+'):
+                logger.debug("volume +")
+            elif got_key == ord('-'):
+                logger.debug("volume -")
+            self.refresher.refresh()
+        # return curdir_path, offset_filelist, selected_line
 
-def process_up():
-    global offset_filelist, selected_line
-    logger.debug("key up")
-    if offset_filelist > 0 and selected_line == 1:
-        offset_filelist = offset_filelist - 1
-    else:
-        selected_line = max(1, selected_line - 1)
-
-
-def process_right(file_list_in_current_dir, our_player):
-    global offset_filelist, curdir_path, selected_line
-    logger.debug("key right")
-    current_offset_in_file_list = selected_line + offset_filelist - 1
-    joined_path = curdir_path.joinpath(
-        Path(file_list_in_current_dir[current_offset_in_file_list]))
-    if selected_line == 1:
-        offset_filelist = 0
-        curdir_path = curdir_path.parent
-    elif joined_path.is_dir():
-        if os.access(joined_path, os.R_OK):
-            offset_filelist = 0
-            curdir_path = joined_path
-            selected_line = 1
+    def process_up(self):
+        logger.debug("key up")
+        if self.current_app_state[0].offset_filelist > 0 and self.current_app_state[0].selected_line == 1:
+            self.current_app_state[0].offset_filelist = self.current_app_state[0].offset_filelist - 1
         else:
-            logger.debug("no read permissions on joined_path, TODO notify user")
-    else:
-        current_queue = play_queue.PlayQueue(
-            curdir_path, file_list_in_current_dir, current_offset_in_file_list)
-        our_player.play_new_queue(current_queue)
+            self.current_app_state[0].selected_line = max(1, self.current_app_state[0].selected_line - 1)
 
+    def process_right(self, file_list_in_current_dir, our_player):
+        logger.debug("key right")
+        current_offset_in_file_list = self.current_app_state[0].selected_line + self.current_app_state[0].offset_filelist - 1
+        joined_path = self.current_app_state[0].curdir_path.joinpath(
+            Path(file_list_in_current_dir[current_offset_in_file_list]))
+        if self.current_app_state[0].selected_line == 1:
+            self.current_app_state[0].offset_filelist = 0
+            self.current_app_state[0].curdir_path = self.current_app_state[0].curdir_path.parent
+        elif joined_path.is_dir():
+            if os.access(joined_path, os.R_OK):
+                self.current_app_state[0].offset_filelist = 0
+                self.current_app_state[0].curdir_path = joined_path
+                self.current_app_state[0].selected_line = 1
+            else:
+                logger.debug("no read permissions on joined_path, TODO notify user")
+        else:
+            current_queue = play_queue.PlayQueue(
+                self.current_app_state[0].curdir_path, file_list_in_current_dir, current_offset_in_file_list)
+            our_player.play_new_queue(current_queue)
 
-def process_down(coord_max_y, file_list_in_current_dir):
-    global offset_filelist, selected_line
-    logger.debug("key down")
-    # if we are at the last entry and there still are entries
-    if (coord_max_y + offset_filelist) < len(file_list_in_current_dir) \
-            and selected_line == coord_max_y:
-        offset_filelist = offset_filelist + 1
-    else:
-        selected_line = min(min(coord_max_y, len(
-            file_list_in_current_dir)), selected_line + 1)
-    # selected_line = min(coord_max_y, selected_line + 1)
+    def process_down(self, coord_max_y, file_list_in_current_dir):
+        logger.debug("key down")
+        # if we are at the last entry and there still are entries
+        if (coord_max_y + self.current_app_state[0].offset_filelist) < len(file_list_in_current_dir) \
+                and self.current_app_state[0].selected_line == coord_max_y:
+            self.current_app_state[0].offset_filelist = self.current_app_state[0].offset_filelist + 1
+        else:
+            self.current_app_state[0].selected_line = min(min(coord_max_y, len(
+                file_list_in_current_dir)), self.current_app_state[0].selected_line + 1)
+        # selected_line = min(coord_max_y, selected_line + 1)
 
 
 curses.wrapper(main)
