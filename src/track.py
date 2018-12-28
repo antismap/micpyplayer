2ed24f3797ab856e58af467d0128b587e80525dc
michael <michael.thenault@gmail.com>
2018-12-28 16:14:16 +0100

    fix not showing id3 tags

--- src/track.py
+++ src/track.py
@@ -4,19 +4,20 @@ import vlc
 class Track(object):
 
     def __init__(self, logger, instance, mediapath):
-        self.path = mediapath
-        self.fullpath = str(mediapath.resolve())
-        self.instance = instance
-        self.logger = logger
-        self.logger.debug("self.fullpath: %s", self.fullpath)
-
-        self.media = self.instance.media_new(self.fullpath)
-        self.has_id3 = self.parse_new_track(self.media)
 
         self.artist = None
         self.album = None
         self.title = None
         self.track_nb = None
+
+        self.path = mediapath
+        self.fullpath = str(mediapath.resolve())
+        self.instance = instance
+        self.logger = logger
+        self.logger.debug("track init: self.fullpath: %s", self.fullpath)
+
+        self.media = self.instance.media_new(self.fullpath)
+        self.has_id3 = self.parse_new_track(self.media)
 
     def parse_new_track(self, media):
         media.parse()
@@ -25,15 +26,16 @@ class Track(object):
         self.title = media.get_meta(vlc.Meta.Title)
         # Display a little symbol instead of the track number if it's unknown
         self.track_nb = media.get_meta(vlc.Meta.TrackNumber) or "*"
-
+        self.logger.debug(
+            "parsed track with nb {s.track_nb} artist {s.artist} title {s.title} album {s.album}".format(s=self))
         # Display track info if we have at least an artist and a title,
         # else display the file name
+        self.logger.debug("end parse_new_track")
         return self.artist and self.title
 
     def get_track_info_line(self):
         if self.has_id3:
-            return "{s.track_nb} - {s.artist} - {s.title} ({s.album})"\
-                   .format(s=self)
+            return "{s.track_nb} - {s.artist} - {s.title} ({s.album})".format(s=self)
         else:
             return self.fullpath
 
